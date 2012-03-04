ca8fdd2470a405dce5d6d6f47895159395e3141f
onionjake <jake.w@utah.edu>
2012-03-04 01:40:57 -0700

    Basic page_action chromium plugin. Copy in doh.js.

---
+++ chromium/content.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2012 Jake Willoughby
+// 
+// This file is part of DOH.
+// 
+// DOH is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// DOH is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with DOH.  See gpl3.txt. If not, see <http://www.gnu.org/licenses/>.
+
+var fields = $("input[type=password]");
+if (fields.size() > 0) {
+  // Notify background page
+  chrome.extension.sendRequest({}, function(response) {});
+  
+  fields.each(function(i,e) {
+    $(e).val("hello");
+  });
+}
