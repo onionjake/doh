c0bfad736d8029128eed0ffee5a4cb42ffdd69b3
onionjake <jake.w@utah.edu>
2012-10-22 13:01:19 -0600

    closer firefox version, still getting exception

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -2,8 +2,26 @@ const widgets = require("widget");
 const tabs = require("tabs");
 const self = require("self");
 
+function parse_hostname (url) {
+  var host = url.match(/^[A-Za-z]+:\/\/([-A-Za-z0-9]+\.)*([-A-Za-z0-9]*\.[-A-Za-z0-9]+)/);
+  if (!(2 in host)) {
+    console.log("Error parsing url");
+    return null;
+  }
+  return host[2];
+}
+
 var master_panel = require("panel").Panel({
-  contentURL: self.data.url("popup.html")
+  contentURL: self.data.url("popup.html"),
+  contentScriptFile: [self.data.url("2.5.3-crypto-sha1-hmac-pbkdf2.js"),
+                      self.data.url("2.5.3-crypto-md5.js"),
+                      self.data.url("2.5.3-crypto-sha256.js"),
+                      self.data.url("jquery-1.8.2.js"),
+                      self.data.url("doh.js"),
+                      self.data.url("insecure.js"),
+                      self.data.url("doh_ui.js"),
+                      self.data.url("domain_specs.js"),
+                      self.data.url("popup.js")]
 });
 
 var widget = widgets.Widget({
@@ -21,21 +39,19 @@ tabs.on("ready",function (tab) {
       self.data.url("content.js")]
   });
   worker.port.on("requestPassword", function (id) {
-    var host = tab.url.match(/^[A-Za-z]+:\/\/([-A-Za-z0-9]+\.)*([-A-Za-z0-9]*\.[-A-Za-z0-9]+)/);
-    if (!(2 in host)) {
-      console.log("Error parsing url");
-    }
-    else {
-      // Ask the DOH_UI that lives in the popup.
-      console.log("Add-on requesting from panel");
-      master_panel.port.emit("requestPassword",host[2],id);
-    }
+    // Ask the DOH_UI that lives in the popup.
+    console.log("Add-on requesting from panel");
+    master_panel.port.emit("requestPassword",parse_hostname(tab.url),tab.index,id);
   });
 });
 
-master_panel.port.on("getPassword", function(pwd,id) {
+master_panel.port.on("getPassword", function(host,tab_index,pwd,input_id) {
   console.log("add-on send password to tab");
-  tab.port.emit("getPassword", id, pwd);
+  var mytab = tabs[tab_index];
+  // Make sure the tabs didn't flip around on us
+  if (parse_hostname(mytab.url) == host) {
+    mytab.port.emit("getPassword", input_id, pwd);
+  } // TODO: write error message on else
 });
 
 console.log("The add-on is running.");
