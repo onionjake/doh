47d98aba083675778bc656f8460b88bcc29e4118
onionjake <jake.w@utah.edu>
2012-10-26 12:47:38 -0600

    intermessage passing in firefox. breaks chrome

--- firefox/data/popup.js
+++ firefox/data/popup.js
@@ -1,33 +1,38 @@
-// domain_specs is set in domain_specs.js
-DOH_UI.init(domain_specs);
+$('#DOHmaster').keyup(setInfo);
 
-self.port.on("requestPassword", function (host,tab_index,input_id,worker) {
-    console.log("Panel sending password to add-on");
-    console.log("worker " + worker);
-    DOH_UI.setHost(host);
-    console.log(DOH_UI.getSalt());
-    console.log(DOH_UI.getSequence());
-    console.log(DOH_UI.getHasher());
-    console.log(DOH_UI.isSetMaster());
-    self.port.emit("getPassword", host, tab_index, DOH_UI.getPassword(), input_id);
+function getInfo () {
+  console.log("requesting info");
+  self.port.emit("getInfo");
+}
+
+self.port.on("getInfo", function (info) {
+    $("#DOHsalt").val(info.salt);
     });
+
 
 // Load back in values from internal state
 $(document).ready(function () {
-    $("#DOHsalt").val(DOH_UI.getSalt());
-    $("#DOHseq").val(DOH_UI.getSequence());
-    if (DOH_UI.isSetMaster()) {
-    $("#DOHmaster")[0].placeholder = "<unchanged>";
-    }
-    $("input:radio[name=hasher][value=" + DOH_UI.getHasher() + "]").attr('checked', true);
+  getInfo();
+//    $("#DOHseq").val(getSequence());
+//    if (isSetMaster()) {
+//    $("#DOHmaster")[0].placeholder = "<unchanged>";
+//    }
+//    $("input:radio[name=hasher][value=" + getHasher() + "]").attr('checked', true);
     });
 
 function setInfo() {
+  console.log("setInfo");
   // Match top-level domain
   var h = $('input:radio[name=hasher]:checked').val();
-  DOH_UI.setHasher(h);
-  DOH_UI.setSalt($('#DOHsalt').val());
-  DOH_UI.setSequence($('#DOHseq').val());
-  DOH_UI.setMaster($('#DOHmaster').val());
-};
+  var s = $('#DOHsalt').val();
+  var seq = $('#DOHseq').val();
+  var m = $('#DOHmaster').val();
 
+  self.port.emit("setInfo", {
+    hasher: h,
+    salt: s,
+    sequence: seq,
+    master: m
+    });
+
+};
--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,19 +1,17 @@
 const widgets = require("widget");
 const tabs = require("tabs");
 const self = require("self");
-const utils = require("utils");
 
-require("2.5.3-crypto-sha1-hmac-pbkdf2.js");
-require("2.5.3-crypto-md5.js");
-require("2.5.3-crypto-sha256.js");
-require("doh.js");
-require("insecure.js");
-require("doh_ui.js");
-require("domain_specs.js");
+var DOH_UI = require("doh_ui");
+var domain_specs = require("domain_specs").domain_specs;
+//require("2.5.3-crypto-sha1-hmac-pbkdf2.js");
+//require("2.5.3-crypto-md5.js");
+//require("2.5.3-crypto-sha256.js");
+//require("insecure.js");
+//require("doh_ui.js");
+//require("domain_specs.js");
 
-var imports = {};
-utils.import("blahh blah", imports);
-
+DOH_UI.init(domain_specs);
 
 var workers = [];
 function find_worker (tab) {
@@ -63,28 +61,29 @@ tabs.on("ready",function (tab) {
   });
   worker.port.on("requestPassword", function (id) {
     // Ask the DOH_UI that lives in the popup.
-    console.log("Add-on requesting from panel");
-    master_panel.port.emit("requestPassword",parse_hostname(tab.url),tab.index,id);
+    console.log("Worker requested password");
+    // TODO: make sure a tab cannot fake out this regex
+    var host = tab.url.match(/^[A-Za-z]+:\/\/([-A-Za-z0-9]+\.)*([-A-Za-z0-9]*\.[-A-Za-z0-9]+)/);
+    if (!(2 in host)) {
+      console.error("DOH: Could not determine Hostname");
+    }
+    else {
+      console.log("Sending back password");
+      DOH_UI.setHost(host[2]);
+      worker.port.emit("getPassword", id, DOH_UI.getPassword());
+    }
   });
 });
 
-master_panel.port.on("getPassword", function(host,tab_index,pwd,input_id) {
-  console.log("add-on send password to tab");
-  console.log("tab index " + tab_index);
-  var mytab = tabs[tab_index];
-  // Make sure the tabs didn't flip around on us
-  if (parse_hostname(mytab.url) == host) {
-    var worker = find_worker(mytab);
-    if (worker != null) {
-      worker.port.emit("getPassword", input_id, pwd);
-    }
-    else {
-      console.error("Couldn't find worker thread. On " + host);
-    }
-  } // TODO: write error message on else
-  else {
-    console.error("Couldn't find tab " + host);
-  }
-});
+master_panel.port.on("getInfo", function() {
+    console.log("sending info");
+    master_panel.port.emit("getInfo", {
+      salt: DOH_UI.getSalt()
+      });
+    });
+master_panel.port.on("setInfo", function(info) {
+    console.log("setting info");
+    DOH_UI.setSalt(info.salt);
+    });
 
 console.log("The add-on is running.");
